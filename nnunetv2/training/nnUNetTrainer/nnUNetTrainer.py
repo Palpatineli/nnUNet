073b27fdeacbe76fc4dc9b0a36b5d0eba1c2ca37
Keji Li <mail@keji.li>
2025-10-31 22:07:25 -0400

    feat: add federated training round

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -1386,3 +1386,24 @@ class nnUNetTrainer(object):
             self.on_epoch_end()
 
         self.on_train_end()
+
+    def run_federated_train_round(self):
+        self.on_epoch_start()
+
+        self.on_train_epoch_start()
+        train_outputs = []
+        for batch_id in range(self.num_iterations_per_epoch):
+            train_outputs.append(self.train_step(next(self.dataloader_train)))
+
+        self.on_train_epoch_end(train_outputs)
+
+        with torch.no_grad():
+            self.on_validation_epoch_start()
+            val_outputs = []
+            for batch_id in range(self.num_val_iterations_per_epoch):
+
+                # lrs are the same for all workers so we don't need to gather them in case of DDP training
+                val_outputs.append(self.validation_step(next(self.dataloader_val)))
+            self.on_validation_epoch_end(val_outputs)
+
+        self.on_epoch_end()
